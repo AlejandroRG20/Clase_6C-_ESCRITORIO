5dad58c01571a94aa05a19bafa77515ddd13f3da
AlejandroRG <111157823+AlejandroRG20@users.noreply.github.com>
2023-01-23 11:46:54 -0600

    Add files via upload

---
+++ WinFormsApp1/Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            textBox3.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ingr,egr, ut;
+            ingr = double.Parse(textBox1.Text);
+            egr = double.Parse(textBox2.Text);
+            if ((ingr >= 4800 && ingr <= 5000000)&& egr<ingr)
+            {
+                ut = ingr - egr;
+                textBox3.Text = ut.ToString();
+            }
+            else
+            {
+                MessageBox.Show("rango entre 4800 y 500000");
+                textBox1.Text = "";
+                textBox1.Focus();
+                textBox2.Text = "";
+                textBox2.Focus();
+                textBox3.Text = "";
+                textBox3.Focus();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                e.Handled = true;
+                return;
+            }
+        }
+    }
+}
---
+++ WinFormsApp1/MDI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class MDI : Form
+    {
+        Form1 x= new Form1();
+        Form2 y= new Form2();   
+        public MDI()
+        {
+            InitializeComponent();
+        }
+
+
+        private void innsLToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            x.MdiParent = this;
+            x.Show();
+        }
+
+        private void utilidadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            y.MdiParent = this;
+            y.Show();
+        }
+    }
+}
